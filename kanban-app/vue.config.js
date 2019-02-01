b7c8171f1e0a3493e0b89cb031f02963619d140e
Shinya Kumagai <roomful.rooms@gmail.com>
2019-02-01 17:36:49 +0900

    Create minimum kanban-app

---
+++ kanban-app/vue.config.js
@@ -0,0 +1,15 @@
+// require()
+module.exports = {
+  devServer: {
+    before: require("./build/dev-server"),
+    proxy: {
+      "/api": {
+        target: "http://api.yourserivice.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/api": ""
+        }
+      }
+    }
+  }
+};
